538c663101b74b522ca05099b9c915e4e449e9ba
belemlc <belemlc@gmail.com>
2018-02-04 01:12:48 -0200

    ajsutado controller, gravando os dados

--- app/controllers/ImovelController.php
+++ app/controllers/ImovelController.php
@@ -1,6 +1,7 @@
 <?php
 
 use Phalcon\Mvc\Controller;
+use Phalcon\Http\Response;
 
 class ImovelController extends Controller
 {
@@ -9,17 +10,59 @@ class ImovelController extends Controller
 	{
         $this->view->imoveis = Imovel::find();
     }
+
+    public function adicionarAction() {
+	    $this->view->form = new ImovelForm;
+        $response = new Response();
+        if ($this->request->isPost() && $this->request->isAjax()) {
+            $bairro = $this->request->getPost("bairro");
+            $logradouro = Logradouro::find("bairro_id = {$bairro}");
+            $response->setStatusCode(200);
+            $this->response->setContentType('application/json');
+            $response->setJsonContent($logradouro);
+            $response->send();
+            exit;
+        }
+    }
     
 
-    public function adicionarAction()
-	{
+    public function saveAction() {
 	    if ($this->request->isPost()) {
-            $data = $this->request->getPost();
-            $imovel = new Imovel();
 
+	        $id = $this->request->getPost("id", "int");
+
+	        try {
+                $imovel = new Imovel();
+                # Recebe os dados via post e converte para objeto
+                $data = (object) $this->request->getPost();
+                # Setters Imovel
+                $imovel->setCodigo($data->codigo);
+                $imovel->setTipoImovelId($data->tipo_imovel);
+                $imovel->setFilial($data->filial);
+                $imovel->setLogradouroId($data->logradouro);
+                $imovel->setNumero($data->numero);
+                $imovel->setTipoNegocio($data->tipo_negocio);
+                $imovel->setValorVenda($data->valor_venda);
+                $imovel->setValorAluguel($data->valor_aluguel);
+                $imovel->setDormitorios($data->dormitorios);
+                $imovel->setAreaTerreno($data->area_terreno);
+                $imovel->setBanheiros($data->banheiros);
+                $imovel->setVagasGaragem($data->vagas_garagem);
+                $imovel->setTituloImovel($data->titulo_imovel);
+                $imovel->setDescricao($data->descricao);
+                $imovel->setPublicado($data->publicado);
+                $imovel->setDataExpiracao($data->data_expiracao);
+                # Save
+                if ($imovel->save()) {
+                    $this->dispatcher->forward([
+                        "controller" => "imovel",
+                        "action" => "listar"
+                    ]);
+                }
+            } catch (Exception $e) {
+	            echo $e->getMessage();
+            }
         }
-        $this->view->tipos = TipoImovel::find();
-	    $this->view->filiais = Filial::find();
     }
     
     public function editarAction($id)
