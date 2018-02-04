34559f78643cef60a026d9d435403afe08ece249
belemlc <belemlc@gmail.com>
2018-02-04 18:13:17 -0200

    adicionado rotas editar, save e remover

--- app/config/routes.php
+++ app/config/routes.php
@@ -8,6 +8,9 @@ $router->add('/', ['controller' => 'Index', 'action' => 'index'])->setName('site
 
 $router->add('/imoveis', ['controller' => 'Imovel', 'action' => 'listar'])->setName('site.imovel.listar');
 $router->add('/imoveis/adicionar', ['controller' => 'Imovel', 'action' => 'adicionar'])->setName('site.imovel.adicionar');
+$router->add('/imoveis/editar/:int', ['controller' => 'Imovel', 'action' => 'editar', 'params' => 1])->setName('site.imovel.editar');
+$router->add('/imoveis/save', ['controller' => 'Imovel', 'action' => 'save'])->setName('site.imovel.save');
+$router->add('/imoveis/remover/:int', ['controller' => 'Imovel', 'action' => 'remover', 'params' => 1])->setName('site.imovel.excluir');
 
 
 return $router;
